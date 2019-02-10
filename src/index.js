a9442f72a08c2657f6ce1ba6fcfecbd5ff280623
Théo <theominier@TiMac.local>
2019-02-10 19:30:00 +0100

    Ajout de la time machine

--- src/index.js
+++ src/index.js
@@ -33,11 +33,12 @@ function calculateWinner(squares) {
           squares: Array(9).fill(null),
         }],
         xIsNext: true,
+        stepNumber: 0,
       };
     }
 
     handleClick(i) {
-      const history = this.state.history;
+      const history = this.state.history.slice(0, this.state.stepNumber + 1);
       const current = history[history.length - 1];
       const squares = current.squares.slice();
       if (calculateWinner(squares) || squares[i]) {
@@ -46,16 +47,36 @@ function calculateWinner(squares) {
       squares[i] = this.state.xIsNext ? 'X' : 'O';
       this.setState({
         history: history.concat([{
-          squares: squares,
+          squares: squares
         }]),
+        stepNumber: history.length,
         xIsNext: !this.state.xIsNext,
       });
     }
     
+    jumpTo(step) {
+      this.setState({
+        stepNumber: step,
+        xIsNext: (step % 2) === 0,
+      });
+    }
+
     render() {
       const history = this.state.history;
-      const current = history[history.length - 1];
+      const current = history[this.state.stepNumber];
       const winner = calculateWinner(current.squares);
+  
+  
+      const moves = history.map((step, move) => {
+        const desc = move ?
+          'Go to move #' + move :
+          'Go to game start';
+        return (
+          <li key={move}>
+                <button onClick={() => this.jumpTo(move)}>{desc}</button>
+          </li>
+        );
+      });
   
       let status;
       if (winner) {
@@ -74,7 +95,7 @@ function calculateWinner(squares) {
           </div>
           <div className="game-info">
             <div>{status}</div>
-            <ol>{/* TODO */}</ol>
+            <ol>{moves}</ol>
           </div>
         </div>
       );
