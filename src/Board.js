ae805a19a58045907b01e7a10efc59a49853bbbe
Théo <theominier@TiMac.local>
2019-02-10 19:19:10 +0100

    Changement archi + historique
    On passe les composantes de board vers game pour y avoir accès
    Et on y fait des sauvegardes des anciens coups (board)

--- src/Board.js
+++ src/Board.js
@@ -11,67 +11,48 @@ function Square(props) {
   }
 
 export default class Board extends React.Component {
-    constructor(props) {
+    /*constructor(props) {
         super(props);
         this.state = {
           squares: Array(9).fill(null),
           xIsNext: true,
         };
-      }
+      }*/
     
       renderSquare(i) {
         return (
           <Square
-            value={this.state.squares[i]}
-            onClick={() => this.handleClick(i)}
+            value={this.props.squares[i]}
+            onClick={() => this.props.onClick(i)}
           />
         );
       }
 
-      handleClick(i) {
-        const squares = this.state.squares.slice();
-        if (calculateWinner(squares) || squares[i]) {
-          return;
-        }
-        squares[i] = this.state.xIsNext ? 'X' : 'O';
-        this.setState({
-          squares: squares,
-          xIsNext: !this.state.xIsNext,
-        });
-      }
+     
 
       render() {
-        const winner = calculateWinner(this.state.squares);
-        let status;
-        if (winner) {
-          status = 'Winner: ' + winner;
-        } else {
-          status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
-        }
-    
         return (
-        <div>
-          <div className="status">{status}</div>
-          <div className="board-row">
-            {this.renderSquare(0)}
-            {this.renderSquare(1)}
-            {this.renderSquare(2)}
+          <div>
+            <div className="board-row">
+              {this.renderSquare(0)}
+              {this.renderSquare(1)}
+              {this.renderSquare(2)}
+            </div>
+            <div className="board-row">
+              {this.renderSquare(3)}
+              {this.renderSquare(4)}
+              {this.renderSquare(5)}
+            </div>
+            <div className="board-row">
+              {this.renderSquare(6)}
+              {this.renderSquare(7)}
+              {this.renderSquare(8)}
+            </div>
           </div>
-          <div className="board-row">
-            {this.renderSquare(3)}
-            {this.renderSquare(4)}
-            {this.renderSquare(5)}
-          </div>
-          <div className="board-row">
-            {this.renderSquare(6)}
-            {this.renderSquare(7)}
-            {this.renderSquare(8)}
-          </div>
-        </div>
-      );
-    }
+        );
+      }
   }
-  function calculateWinner(squares) {
+  /*function calculateWinner(squares) {
     const lines = [
       [0, 1, 2],
       [3, 4, 5],
@@ -89,4 +70,4 @@ export default class Board extends React.Component {
       }
     }
     return null;
-  }
+  }*/
